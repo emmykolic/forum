64153f92e994af77d81e3e98f3fe0f7524d374db
Emmanuel Okolie <emmanuelokolie550@gmail.com>
2023-05-30 22:51:58 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="css/aos.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hello, world!</title>
+  </head>
+<nav class="navbar position-fixed w-100 top-0 navbar-expand-md navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include_once "header.php"; ?>
+<section class="hero mt-5">
+</section>
+
+<section class="start bg-primary py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="shadow bg-white">
+                    <div class="row">
+                        <div class="col-5 bg-info p-3 d-flex justify-content-center align-items-center">
+                            <i class="fa fa-newspaper fa-3x text-white"></i>
+                        </div>
+                        <div class="col-7 p-3 d-flex justify-content-center align-items-center">
+                            <span class="h5"> 20 <br> Topics</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="shadow bg-white">
+                    <div class="row">
+                        <div class="col-5 bg-success p-3 d-flex justify-content-center align-items-center">
+                        <i class="fa fa-comment fa-3x text-white"></i>
+                        </div>
+                        <div class="col-7 p-3 d-flex justify-content-center align-items-center">
+                        <span class="h5"> 30 <br> Comments</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="shadow bg-white">
+                    <div class="row">
+                        <div class="col-5 bg-danger p-3 d-flex justify-content-center align-items-center">
+                        <i class="fa fa-users fa-3x text-white"></i>
+                        </div>
+                        <div class="col-7 p-3 d-flex justify-content-center align-items-center">
+                            <span class="h5"> 25 <br> Users</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="bg-white p-md-5 p-2 col-md-10 offset-md-1 col-12">
+        <div class="row">
+          <div class="col-md-8">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus fugit sint, aperiam, iusto neque quas non sed nostrum recusandae at praesentium maxime sequi rem deserunt veritatis provident ratione libero numquam.
+          </div>
+          <div class="col-md-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quam a eaque vitae quae officia. Beatae natus commodi expedita in, repellendus fugit inventore impedit blanditiis cupiditate quo eveniet sed soluta.
+          </div>
+        </div>
+      </div>
+    </div>     
+  </div>
+</section>
+<?php include_once "footer.php"; ?>
